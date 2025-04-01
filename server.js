b9263133fa3a4eb048ad9475410dafd16e93998e
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2025-04-01 23:29:51 +0530

    parameterised search

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
  const app = express();
  const db = require('./db');
  const Person = require('./models/person')
+ const menu = require('./menu')
  const bodyParser = require('body-parser')
 app.use(bodyParser.json())
  app.get('/',(req,res)=>{
@@ -34,6 +35,44 @@ res.status(200).json(data);
     
   }
  })
+app.post('/menu',async(req,res)=>{
+  try{
+const menuData = req.body;
+const newMenu = menu(menuData);
+const saveMenu = await newMenu.save()
+console.log('done')
+res.status(200).json(saveMenu)
+  }
+  catch(err){
+    console.log(err);
+    res.status(500).json(saveMenu)
+    
+  }
+})
+app.get('/getd/:workType',async(req,res)=>{
+  try{
+const workType = req.params.workType;
+if(workType == 'waiter'){
+const sendRes =await Person.find({designation:workType});
+console.log('response fetched')
+res.status(200).json(sendRes);
+}
+else{
+  res.status(404).json({
+    error:'input error'
+  })
+}
+  }
+  catch(err){
+    console.log(err);
+    res.status(500).json({error:"internal error"})
+    
+  }
+})
+
+
+
+
  app.listen(3000,()=>{
    console.log('listening on p -3000')
  })
