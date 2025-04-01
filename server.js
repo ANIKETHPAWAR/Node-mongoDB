dd390f3fcb7523975714babfde9d48070d817aca
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2025-04-01 17:32:42 +0530

    get post method , mongoose schema

--- server.js
+++ server.js
@@ -7,19 +7,32 @@ app.use(bodyParser.json())
  app.get('/',(req,res)=>{
     res.send("hello")
  })
- app.post('/post',(req,res)=>{
-const data= req.body
-const newPerson = new Person(data);
-newPerson.save((error,savedPerson)=>{
-  if(error){
-    console.log('Error saving person',error);
-    res.status(500).json({error:'internal server error'})
-  }else{
-    console.log('data saved successfully');
-    res.status(200).json(savedPerson);
-  }
-})
+ app.post('/post',async (req,res)=>{
+try{
+const data = req.body
+const newPerson =new Person(data);
 
+const savedPerson=await newPerson.save()
+console.log('data saved');
+res.status(200).json(savedPerson)
+}catch(err){
+console.log(err);
+res.status(500).json(savedPerson)
+}
+
+ })
+
+ app.get("/getd",async(req,res)=>{
+  try{
+const data = await Person.find();
+console.log('data fetch');
+res.status(200).json(data);
+  }
+  catch(err){
+    console.log(err);
+    res.status(500).json(savedPerson)
+    
+  }
  })
  app.listen(3000,()=>{
    console.log('listening on p -3000')
