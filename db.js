c6f3710b1d7e676bc46f8cef90d94f5fdd868504
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2025-05-10 11:48:38 +0530

    newwww

--- db.js
+++ db.js
@@ -4,9 +4,14 @@
  require('dotenv').config()
 const mongoDb_URL =  process.env.MONGODB_URI ;
 
-mongoose.connect(mongoDb_URL)
-.then(() => console.log("Connected to MongoDB"))
-.catch((err) => console.error("MongoDB connection error:", err));
+mongoose.connect(mongoDb_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+})
+.then(() => {
+    console.log('MongoDB connected');
+})
 if (!mongoDb_URL) {
    console.error("❌ MONGODB_URI is undefined. Check your .env or Render environment variables.");
    process.exit(1);
