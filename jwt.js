b944f6ab9ff9afb3d737b6dfac085d0f7815a9a8
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2025-05-11 12:34:52 +0530

    created jwtAuth middleware

---
+++ jwt.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+//creating middleware function
+const jwtAuthMiddleware = (req,res,next)=>{
+
+    // jwt token extraction from request headers
+    const token = req.headers.authorization.split(' ')[1];
+    if(!token){
+        return res.status(401).json({error:"unauthorized"});
+    }
+    try{
+        // when we get token and we will verify now
+const decodedValue = jwt.verify(token,process.env.JWT_SECRET);
+
+req.user = decodedValue
+next();
+
+
+    }
+catch(err){
+    console.log(err);
+    res.status(401).json({error:'invalid token'})
+}
+
+
+}
+
+module.exports = jwtAuthMiddleware
